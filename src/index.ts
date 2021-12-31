51df201c40a456c3131889d880286beebe8cb05c
Raphael Cavalcanti <46363242+raphaelcavalcanti0@users.noreply.github.com>
2021-12-31 00:06:22 -0300

    configurado o hot reload

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('opa')
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+const port = 8000;
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ status: "agora sim" });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}/status`);
+});
